92749cedddf40833c3b8abacde1f0726be933eaa
Cristian Rojas <cristian.rojas@live.fr>
2025-06-27 17:57:34 +0200

    Compose app within cli

--- Sources/HummingBirdTodoApp.swift
+++ Sources/HummingBirdTodoApp.swift
@@ -1,14 +1,23 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
-// 
+//
 // Swift Argument Parser
 // https://swiftpackageindex.com/apple/swift-argument-parser/documentation
 
 import ArgumentParser
+import Hummingbird
 
 @main
 struct HummingBirdCounterApp: ParsableCommand {
-    mutating func run() throws {
-        print("Hello, world!")
+    @Option(name: .shortAndLong)
+    var hostname: String = "127.0.0.1"
+    
+    @Option(name: .shortAndLong)
+    var port: Int = 8080
+    
+    func run() async throws {
+        let store = InMemoryCounterStore()
+        let config = ApplicationConfiguration(address: .hostname(self.hostname, port: self.port), serverName: "Hummingbird")
+        return try await AppComposer.execute(with: config, store: store).runService()
     }
 }
