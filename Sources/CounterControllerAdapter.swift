6abfcda624ddfe585f6dc3a053a1f9bcc605d7d4
Cristian Rojas <cristian.rojas@live.fr>
2025-06-27 17:51:04 +0200

    Move `CounterControllerAdapter` to prod

--- Sources/CounterControllerAdapter.swift
+++ Sources/CounterControllerAdapter.swift
@@ -1,21 +1,24 @@
 // © 2025  Cristian Felipe Patiño Rojas. Created on 27/6/25.
 
 import Hummingbird
-import HummingBirdCounterApp
 
-struct CounterControllerAdapter: @unchecked Sendable {
-    let controller: CounterController
-    @Sendable func get(request: Request, context: BasicRequestContext) async throws -> Response {
+public struct CounterControllerAdapter: @unchecked Sendable {
+    private let controller: CounterController
+    public init(controller: CounterController) {
+        self.controller = controller
+    }
+    
+    @Sendable public func get(request: Request, context: BasicRequestContext) async throws -> Response {
         let body = try await controller.get()
         return try ResponseGeneratorEncoder.execute(body, from: request, context: context)
     }
     
-    @Sendable func postIncrease(request: Request, context: BasicRequestContext) async throws -> Response {
+    @Sendable public func postIncrease(request: Request, context: BasicRequestContext) async throws -> Response {
         let body = try await controller.postIncrease()
         return try ResponseGeneratorEncoder.execute(body, from: request, context: context)
     }
     
-    @Sendable func postDecrease(request: Request, context: BasicRequestContext) async throws -> Response {
+    @Sendable public func postDecrease(request: Request, context: BasicRequestContext) async throws -> Response {
         let body = try await controller.postDecrease()
         return try ResponseGeneratorEncoder.execute(body, from: request, context: context)
     }
